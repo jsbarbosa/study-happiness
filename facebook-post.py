2a33f93d8a751b546dd211cbed13b8e48821333c
Juan Barbosa <js.barbosa10@uniandes.edu.co>
2016-12-31 20:07:33 -0500

    new years

--- facebook-post.py
+++ facebook-post.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  facebook_post.py
+#  facebook-post.py
 #  
 #  Copyright 2016 Juan Barbosa <juan@Lenovo-U410>
 #  
@@ -21,20 +21,40 @@
 #  MA 02110-1301, USA.
 
 import facebook
+from datetime import datetime
+from apscheduler.schedulers.blocking import BlockingScheduler as Scheduler
 
 def main():
     with open('access_token.txt', 'r') as f:
         access_token = f.readline()
+    
 
     message = """ print('Hello world!')
     Happy New Year!.
     
     
-    If you are seeing this, I made it work.
-    Go to  
+    If you are seeing this: I made it work, and I'm probably sleeping right now. 
     """
+
+    
     api = facebook.GraphAPI(access_token)
-    api.put_object("me", "feed", message = message)
+        
+    api.put_object("me", "feed", message = "Test")
+    api.put_object("me", "feed", message=message,  
+                             link="https://github.com/jsbarbosa/study-happiness/blob/master/facebook-post.py",  
+                             picture="http://www.ecorobotix.com/old/wp-content/uploads/2016/01/2016.png",    
+                             name = "GitHub Facebook post",    
+                             description = "I'm probably sleeping",
+                             privacy = '{"value":"EVERYONE"}'
+                             )
+
+    print("Ended")
+    sched.shutdown(wait=False)
 
 if __name__ == "__main__":
-    main()
+    sched = Scheduler() # Start the scheduler
+  
+    exec_date = datetime(2017, 1, 1, 0, 0, 0)
+    sched.add_job(main, "date", run_date=exec_date)    
+    sched.start()
+    
